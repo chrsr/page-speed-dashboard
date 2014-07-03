0a9ac96b1159b0446ac6dcadff0dc84278a1f741
Chris Roberts <chris.roberts@wotifgroup.com>
2014-07-03 14:57:02 +1000

    Configure the browser size for screen capture.

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,30 +1,39 @@
 var casper = require('casper').create();
 var fs = require('fs');
 
-urls = JSON.parse(fs.read('pages.json'));
-validatedUrls = [];
+urls = JSON.parse(fs.read('../pages.json'));
 var i = -1;
 
-casper.start('http://www.wotif.com');
+var dimentions = {
+    width: 1280,
+    height: 1024
+};
+
+casper.start();
+
+// casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)');
+casper.viewport(dimentions.width, dimentions.height);
 
 casper.then(function () {
     this.each(urls, function () { 
         i += 1;
         this.thenOpen((urls[i].url), function () {
             this.echo(this.getCurrentUrl());
-            this.capture('screenshots/' + urls[i].name + new Date().getTime() + '.png', {
+            this.capture('screenshots/' + this.getTitle() + new Date().getTime() + '.png', {
                 top: 0,
                 left: 0,
-                width: 1024,
-                height: 768
+                width: dimentions.width,
+                height: dimentions.height
             });
         });
         if (urls[i].thenClick) {
             this.thenClick(urls[i].thenClick, function() {
                 this.echo(this.getCurrentUrl());
-                this.capture('screenshots/' + urls[i].name + new Date().getTime() + '.png', {
+                this.capture('screenshots/' + this.getTitle() + new Date().getTime() + '.png', {
                     top: 0,
-                    left: 0
+                    left: 0,
+                    width: dimentions.width,
+                    height: dimentions.height
                 });
             });
         }
